9776c2c79eeeaffd7d5201620aae337decf0d138
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-06-06 12:16:52 +0500

    for window.cpp applicatiom, we initialized the version of glfw for the console, and created the basic struct of the window

--- apps/window_app/window.cpp
+++ apps/window_app/window.cpp
@@ -5,12 +5,16 @@
 
 int main()
 {
+    // initalizing the version of glfw for the console
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // creating a window
+    // delcaring a pointer named window that points to the GLFW window.
+    // where window calles a creatwindow funciton and sets its size, and name.
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
